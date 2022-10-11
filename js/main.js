b1c45f4889fb6a4a202f8b23966caf1afefb2a06
git-rr <devel-rr@proton.me>
2022-10-11 13:03:21 +0200

    updated functionality: matched blocks remain visible

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let col = 4;
 let row = 2;
 
 const all_blocks = [1,2,3,4];       // all possible blocks
-let remainingBlocks = [];
+let remainingBlocks = [];           // blocks to be placed
 let blockMap = [];                  // placement
 let selectedBlocks = [];            // two blocks chosen
 
@@ -31,47 +31,90 @@ blocks.forEach((block)=>{
         if(selectedBlocks.length>=2){
             // run function to check whether values match in blockMap
             // and/or do the following
-            setTimeout(() => {
-                blocks.forEach((block)=>{
-                    block.children[0].style.display = 'none';
-                });
-            }, 800);
-            selectedBlocks = []; // reset selection
+            checkIfBlocksMatch();
+            
         }
 
     });
 });
 
+function checkIfBlocksMatch(){
+    if(blockMap[selectedBlocks[0]]===blockMap[selectedBlocks[1]]){
+        // correct blocks selected
+        // update score
+        
+        document.querySelector(`#main-content :nth-child(${selectedBlocks[0]+1})`).classList += " found";
+        document.querySelector(`#main-content :nth-child(${selectedBlocks[1]+1})`).classList += " found";
+        
+
+    }else{
+        // incorrect blocks selected
+        setTimeout(() => {
+            blocks.forEach((block)=>{
+                if(!block.classList.contains("found")){
+                    block.querySelector(" :nth-child(1)").style.display = 'none';
+                }
+            });
+        }, 800);
+    }
+
+    selectedBlocks = []; // reset selection
+}
 
 function generateBlockMap(){
     // generate random placement of blocks and place in blockMap
 
     all_blocks.forEach((e)=>{
-        remainingBlocks.push(e);
-        remainingBlocks.push(e);
-    });                                 // blocks to be placed
+        for (let r = 0; r < row; r++) {
+            remainingBlocks.push(e);
+        }
+    });
     
-    for (let r = 0; r < row; r++) {
-        blockMap.push([]);
-        for (let c = 0; c < col; c++) {
+        for (let c = 0; c < row*col; c++) {
             let randomBlock = Math.floor(Math.random()*remainingBlocks.length)+0; // 0 to 7 index
-            blockMap[r][c] = remainingBlocks[randomBlock];
+            blockMap[c] = remainingBlocks[randomBlock];
             remainingBlocks.splice(randomBlock,1); // remove element
         }
-    }
-    
-    console.log(blockMap);
+    // 2-D array
+    // for (let r = 0; r < row; r++) {
+    //     blockMap.push([]);
+    //     for (let c = 0; c < col; c++) {
+    //         let randomBlock = Math.floor(Math.random()*remainingBlocks.length)+0; // 0 to 7 index
+    //         blockMap[r][c] = remainingBlocks[randomBlock];
+    //         remainingBlocks.splice(randomBlock,1); // remove element
+    //     }
+    // }
 }
 
 function displayBlocks(){
-    blockMap.forEach((blockRow)=>{
-        blockRow.forEach((e)=>{
-            main_content.innerHTML += `
-                <div class="block">
-                    <img class="face-img" src="images/face-${e}.png" alt="">
-                </div>
-            `;
-        })
+    blockMap.forEach((block)=>{
+        main_content.innerHTML += `
+            <div class="block">
+                <img class="face-img" src="images/face-${block}.png" alt="">
+            </div>
+        `;
     });
+    // blockMap.forEach((blockRow)=>{
+    //     blockRow.forEach((e)=>{
+    //         main_content.innerHTML += `
+    //             <div class="block">
+    //                 <img class="face-img" src="images/face-${e}.png" alt="">
+    //             </div>
+    //         `;
+    //     })
+    // });
+
+    // debugging
+    let str = "";
+    for (let r = 0; r < row; r++) {
+        for (let c = 0; c < col; c++) {
+            str += blockMap[(r*col)+c] +  " ";
+        }
+        str += "\n";
+    }
+    console.log("Block Map:")
+    console.log("-----------")
+    console.log(str);
+    console.log("-----------")
 }
 
