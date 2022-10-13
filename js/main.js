d119b06f08bb1ba2ffaacf3612df7ac4dab3ac15
git-rr <devel-rr@proton.me>
2022-10-13 09:59:51 +0200

    Fixed bug: only two blocks can be selected at a time; single block cannot be selected more than once

--- js/main.js
+++ js/main.js
@@ -3,73 +3,100 @@ options: 2x2, 4x4, 6x6
 */
 
 
-const main_content = document.getElementById("main-content");
-const scoreValue = document.getElementById("scoreValue");
+//const mainContent = document.getElementById("mainContent");
+//const scoreCount = document.getElementById("scoreCount");
+//const tryCount = document.getElementById("tryCount");
 
 let col = 4;
 let row = 2;
 
-const all_blocks = [1,2,3,4];       // all possible blocks
-let remainingBlocks = [];           // blocks to be placed
-let blockMap = [];                  // placement
-let selectedBlocks = [];            // two blocks chosen
+const all_blocks = [1,2,3,4];            // all possible blocks
+let remainingBlocks = [];                // blocks to be placed
+let blockMap = [];                       // placement
+let selectedBlocks = [];                 // two blocks chosen
 
-let score = 0;
+let scoreValue = 0;
+let tryValue = 0;
 
 
-scoreValue.innerText = score;
+scoreCount.innerText = scoreValue;
+tryCount.innerText = tryValue;
+
 generateBlockMap();
 displayBlocks();
 
 const blocks = document.querySelectorAll(".block");
-const blocksArr = Array.from(blocks); // or  arr = [...nodeList]
+const blocksArr = Array.from(blocks);                   // or  arr = [...nodeList]
 //console.log(blocks)
 
-//let i = 0
+let blockClicked = (event) => {
+    const block = event.target;
+    block.removeEventListener('click', blockClicked);   // same block cannot be selected more than once
+    block.children[0].style.display = 'block';
+    console.log(blocksArr.indexOf(block));
+    selectedBlocks.push(blocksArr.indexOf(block));
+    
+    event.target.style.backgroundColor = "red";         // shows blocks that have been selected
 
-blocks.forEach((block)=>{
-    block.addEventListener('click', ()=>{
-        block.children[0].style.display = 'block';
-        console.log(blocksArr.indexOf(block));
-        selectedBlocks.push(blocksArr.indexOf(block));
+    if(selectedBlocks.length>=2){
+        removeBlockEventListeners();                    // prevents more than 2 blocks from being clicked
+        checkIfBlocksMatch();
+        setTimeout(addBlockEventListeners, 400);
+    }
 
-        if(selectedBlocks.length>=2){
-            // run function to check whether values match in blockMap
-            // and/or do the following
-            checkIfBlocksMatch();
-            
+}
+
+addBlockEventListeners();
+
+function addBlockEventListeners(){
+    blocks.forEach((block)=>{
+        if (!block.classList.contains('found')) {
+            block.addEventListener('click', blockClicked);
         }
-
     });
-});
+}
+
+function removeBlockEventListeners(){
+    blocks.forEach((block)=>{
+        block.removeEventListener('click', blockClicked);
+    });
+}
 
 function checkIfBlocksMatch(){
+    tryValue++;
+    tryCount.innerText = tryValue;
+
+    const block1 = blocks[selectedBlocks[0]];
+    const block2 = blocks[selectedBlocks[1]];
+
     if(blockMap[selectedBlocks[0]]===blockMap[selectedBlocks[1]]){
-        // correct blocks selected
-        // update score
+        // blocks matched
+        block1.classList += " found";                   // add class to keep blocks visible
+        block2.classList += " found";
         
-        document.querySelector(`#main-content :nth-child(${selectedBlocks[0]+1})`).classList += " found";
-        document.querySelector(`#main-content :nth-child(${selectedBlocks[1]+1})`).classList += " found";
+        scoreValue++;
+        scoreCount.innerText = scoreValue;
+        console.log(`Score: ${scoreValue}`);
         
-        score++;
-        scoreValue.innerText = score;
-        console.log(`Score: ${score}`);
-        
-        if(score>=all_blocks.length){
-            console.log('Well Done!')
+        if(scoreValue>=all_blocks.length){              // end of game
+            console.log('Well Done!');
+            scoreCount.style.color = "red";
+            tryCount.style.color = "red";
         }
     }else{
-        // incorrect blocks selected
+        // blocks don't match
         setTimeout(() => {
-            blocks.forEach((block)=>{
-                if(!block.classList.contains("found")){
-                    block.querySelector(" :nth-child(1)").style.display = 'none';
-                }
-            });
+            block1.querySelector(" :nth-child(1)").style.display = 'none';
+            block2.querySelector(" :nth-child(1)").style.display = 'none';
+            // blocks.forEach((block)=>{
+            //     if(!block.classList.contains("found")){
+            //         block.querySelector(" :nth-child(1)").style.display = 'none';
+            //     }
+            // });
         }, 800);
     }
 
-    selectedBlocks = []; // reset selection
+    selectedBlocks = [];                                // reset selection
 }
 
 function generateBlockMap(){
@@ -84,7 +111,7 @@ function generateBlockMap(){
         for (let c = 0; c < row*col; c++) {
             let randomBlock = Math.floor(Math.random()*remainingBlocks.length)+0; // 0 to 7 index
             blockMap[c] = remainingBlocks[randomBlock];
-            remainingBlocks.splice(randomBlock,1); // remove element
+            remainingBlocks.splice(randomBlock,1);          // remove element
         }
     // 2-D array
     // for (let r = 0; r < row; r++) {
@@ -99,7 +126,7 @@ function generateBlockMap(){
 
 function displayBlocks(){
     blockMap.forEach((block)=>{
-        main_content.innerHTML += `
+        mainContent.innerHTML += `
             <div class="block">
                 <img class="face-img" src="images/face-${block}.png" alt="">
             </div>
@@ -107,7 +134,7 @@ function displayBlocks(){
     });
     // blockMap.forEach((blockRow)=>{
     //     blockRow.forEach((e)=>{
-    //         main_content.innerHTML += `
+    //         mainContent.innerHTML += `
     //             <div class="block">
     //                 <img class="face-img" src="images/face-${e}.png" alt="">
     //             </div>
