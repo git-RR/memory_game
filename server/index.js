6bd0990516cc483f0ce5f635221193a09d3b41bb
git-rr <devel-rr@proton.me>
2022-10-28 21:07:52 +0200

    connected to mongodb atlas.

--- server/index.js
+++ server/index.js
@@ -1,73 +1,115 @@
 const express = require("express");
-const { sample } = require("underscore");
-//const {MongoClient} = require("mongodb");
+// const { sample } = require("underscore");
+const {MongoClient, ObjectId} = require("mongodb");
 
 const app = express();
 
 const port = process.env.PORT || 5001;
+
+const DATABASE_NAME             = "new_game";
+const COLLECTION_NAME           = "high-scores"; 
+const high_score_list_limit     = 10;
+
 app.listen(port, ()=>{console.log(`listening @ port ${port}`)});
 app.use(express.json());
 
 app.use(express.static('../'));     // for testing only
 
-app.get('/api', (request, response)=>{
-	// response.json({message: 'the database is connected.'});
-    response.json(sample_data)
+app.get('/api', async (request, response)=>{
+    const client = await main();
+	const result = await getHighScores(client);
+    //response.json(sample_data)
+    response.json(result);
 });
 
 app.post("/api", async (request, response)=>{
     const data = request.body;
-    //await main(data);
-
-    console.log('New High Score:');
-    console.log(data);
-    sample_data.push({name:data.name, score:data.score, id:''});
-
-    console.log('Added High Score to List:');
-    console.log(sample_data);
-
-    // remove lowest score
-    sample_data.forEach((scoreObj)=>{
-        if(scoreObj.id === data.id){
-            sample_data.splice(sample_data.indexOf(scoreObj), 1);
-        }
-    });
-
-    console.log('New High Score List:');
-    console.log(sample_data);
-    response.json({data: 'high score list updated!'});
-});
-
-
-async function main(data){
     const newHighScore = {name:data.name, score:data.score};
     const id = data.id;
+    const client = await main();
+    await addNewHighScore(client, newHighScore);
+    await deleteOldHighScore(client, id);
 
-    const password = process.env.DB_PWD || "";
-    const uri = `mongodb+srv://robot-army:<password>@cluster0.cxoh44a.mongodb.net/?retryWrites=true&w=majority`;
+    /* start test code */
+    // console.log('New High Score:');
+    // console.log(data);
+    // sample_data.push({name:data.name, score:data.score, id:''});
+
+    // console.log('Added High Score to List:');
+    // console.log(sample_data);
+
+    // // remove lowest score
+    // sample_data.forEach((scoreObj)=>{
+    //     if(scoreObj.id === data.id){
+    //         sample_data.splice(sample_data.indexOf(scoreObj), 1);
+    //     }
+    // });
+
+    // console.log('New High Score List:');
+    // console.log(sample_data);
+    response.json({data: 'high score list updated!'});
+    /* end test code */
+});
+main()
+async function main(){
+    // const newHighScore = {name:data.name, score:data.score};
+    // const id = data.id;
+
+    const password = process.env.DB_PWD || "yQnn0dQGTdd4Sy2766";
+    const uri = `mongodb+srv://robot-army:${password}@cluster0.cxoh44a.mongodb.net/?retryWrites=true&w=majority`;
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
+        // await addNewHighScore(client, newHighScore);
+        // await deleteOldHighScore(client, id);
+        
+        // await resetHighScoreCollection(client);
+        // getHighScores(client)
+        // const result = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).deleteOne({_id:ObjectId('635befc24379eb3424036f8a')});
 
-        await addNewHighScore(client, newHighScore);
-        await deleteOldHighScore(client, id);
+        return client;
 
     } catch (error) {
         console.error(error)
-    }finally{
-        await client.close();
     }
+    // finally{
+    //     await client.close();
+    // }
 }
 
 async function addNewHighScore(client, newHighScore){
-	const result = await client.db("DATABASE_NAME").collection("COLLECTION_NAME").insertOne(newHighScore);
+	const result = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).insertOne(newHighScore);
 	console.log(`New high score added with id: ${result.insertedId}`);
 }
 
 async function deleteOldHighScore(client, id){
-    const result = await client.db("DATABASE_NAME").collection("COLLECTION_NAME").deleteOne({_id:id});
+    const result = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).deleteOne({_id:ObjectId(id)});
     console.log(`${result.deleteCount} document(s) was/were deleted.`)
+}
+
+async function getHighScores(client){
+	const cursor = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).find({}).sort({score: -1}).limit(high_score_list_limit);
+    const result = await cursor.toArray();
+    console.log(result);
+    return result;
+}
+
+
+/* test code */
+
+// main();
+
+async function resetHighScoreCollection(client){
+    let defaultScores = [];
+    for (let i=1; i<=high_score_list_limit; i++) {
+        defaultScores.push({name:'player unknown',score:0});
+    }
+    const result_delete = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).deleteMany({}); // clear database
+    console.log(`${result_delete.deletedCount} documents deleted.`);
+    const result = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).insertMany(defaultScores);
+    console.log(`High Score List Length : ${result.insertedCount}`);
+    //console.log(`ID's : ${result.insertedIds}`);
 }
 
 let sample_data = [
