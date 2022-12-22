849d0ce8aaaa1dccd84fb83e6fa91614177390ff
git-rr <devel-rr@proton.me>
2022-12-22 07:10:41 +0200

    code clean-up
    removed all test code from index.js

--- server/index.js
+++ server/index.js
@@ -30,6 +30,79 @@ async function main(){
     }
 }
 
+/*** USER SIGN-UP/IN ***/
+
+app.post("/api/user-cred", async (request, response)=>{
+    // create new user
+
+    const data = request.body;
+
+    const encryptionKey = Math.floor(Math.random() * (61 + 61 + 1)) - 61; //(max - min + 1)) + min
+
+    const newUserCred = {
+        playerName: data.playerName,
+        passphrase: data.passphrase,
+        encryptionKey : encryptionKey,
+    };
+
+    const loginFlag = (data.login);
+    const client = await main();
+    const userCred = await getUserCred( client, data.playerName );
+
+    if( userCred === null ) {
+        // not found
+
+        if( loginFlag ) {
+            // trying to log in
+            response.json({data: 01, status:'Login Failed.'});
+        } else {
+            // trying to create new user
+            await addNewUser(client, newUserCred);
+            response.json({
+                data: 02, 
+                status:'New User Created.', 
+                passphrase: encrypt(newUserCred.passphrase, newUserCred.encryptionKey),
+            });
+        }
+
+    } else {
+        // found
+
+        if( loginFlag ) {
+            // trying to log in
+
+            if( userCred.passphrase === newUserCred.passphrase ){
+                // passphrase matched
+                response.json({
+                    data: 03, 
+                    status:'Login Succeeded.',
+                    passphrase: encrypt(userCred.passphrase, userCred.encryptionKey),
+                });
+
+            } else {
+                // passphrase mismatch
+                response.json({data: 04, status:'Login Failed.'});
+            }
+            
+        } else {
+            // trying to create new user
+            response.json({data: 05, status:'Username is Taken.'});
+        }
+    }
+    
+    return;
+});
+
+async function addNewUser(client, newUserCreds){
+	const result = await client.db(DATABASE).collection(COLLECTION_USER_CREDS).insertOne(newUserCreds);
+	// console.log(`New user added with id: ${result.insertedId}`);             // debug
+}
+
+async function getUserCred(client, player_name){
+	const result = await client.db(DATABASE).collection(COLLECTION_USER_CREDS).findOne({playerName: player_name});
+    return result;
+}
+
 /*** HIGH SCORE ***/
 
 app.get('/api/highscore', async (request, response)=>{
@@ -59,14 +132,13 @@ app.post("/api/highscore", async (request, response)=>{
             response.json({data: 12, message: `High Score Submitted.`});
         } else {
             // passphrase does not match
-            response.json({data: 10, message: `Failed To Authenticate. Aborted High Score Submit.`});
             // console.log(`failed to authenticate`);                   // debug
+            response.json({data: 10, message: `Failed To Authenticate. Aborted High Score Submit.`});
         }
     } else {
         // user not found
-        response.json({data: 11, message: `User Not Found. Aborted High Score Submit.`});
         // console.log(`user not found`);                               // debug
-
+        response.json({data: 11, message: `User Not Found. Aborted High Score Submit.`});
     }
 
     return;
@@ -171,137 +243,46 @@ app.put("/api/save-game", async (request, response)=>{
     }
 });
 
-app.post("/api/user-cred", async (request, response)=>{
-    // create new user
-
-    const data = request.body;
-
-    const encryptionKey = Math.floor(Math.random() * (61 + 61 + 1)) - 61; //(max - min + 1)) + min
-
-    const newUserCred = {
-        playerName: data.playerName,
-        passphrase: data.passphrase,
-        encryptionKey : encryptionKey,
-    };
-
-    const loginFlag = (data.login);
-    const client = await main();
-    const userCred = await getUserCred( client, data.playerName );
-
-    if( userCred === null ) {
-        // not found
-
-        if( loginFlag ) {
-            // trying to log in
-            response.json({data: 01, status:'Login Failed.'});
-        } else {
-            // trying to create new user
-            await addNewUser(client, newUserCred);
-            response.json({
-                data: 02, 
-                status:'New User Created.', 
-                passphrase: encrypt(newUserCred.passphrase, newUserCred.encryptionKey),
-            });
-        }
-
-    } else {
-        // found
-
-        if( loginFlag ) {
-            // trying to log in
-
-            if( userCred.passphrase === newUserCred.passphrase ){
-                // passphrase matched
-                response.json({
-                    data: 03, 
-                    status:'Login Succeeded.',
-                    passphrase: encrypt(userCred.passphrase, userCred.encryptionKey),
-                });
-
-            } else {
-                // passphrase mismatch
-                response.json({data: 04, status:'Login Failed.'});
-            }
-            
-        } else {
-            // trying to create new user
-            response.json({data: 05, status:'Username is Taken.'});
-        }
-    }
-    
-    return;
-});
-
-// save game database
 async function addNewSaveGameData(client, newSaveGameData){
-    // NEW SAVE GAME
 	const result = await client.db(DATABASE).collection(COLLECTION_SAVE_DATA).insertOne(newSaveGameData);
-	console.log(`New save game added with id: ${result.insertedId}`);
+	// console.log(`New save game added with id: ${result.insertedId}`);        // debug
 }
 
 async function updateSaveGameData(client, user, new_data){
-    // SAVE GAME
-    // user = { playerName : 'player1' }
-    // new_data = { playerName : 'player1', ... }
-
     const result = await client.db(DATABASE).collection(COLLECTION_SAVE_DATA).updateOne(user, new_data);
-    // console.log('1 document updated...');
-    // console.log(result);
     return result;
 }
 
 async function getSaveGameData(client, player_name){
-    // LOAD GAME
 	const result = await client.db(DATABASE).collection(COLLECTION_SAVE_DATA).findOne({playerName: player_name});
-    console.log('loaded game : ');
-    console.log(result);
     return result;
 }
 
-// user cred's database
+/*** ENCRYPTION ***/
 
-async function addNewUser(client, newUserCreds){
-    // NEW USER
-	const result = await client.db(DATABASE).collection(COLLECTION_USER_CREDS).insertOne(newUserCreds);
-	console.log(`New user added with id: ${result.insertedId}`);
-}
-
-async function getUserCred(client, player_name){
-    // GET USER
-	const result = await client.db(DATABASE).collection(COLLECTION_USER_CREDS).findOne({playerName: player_name});
-    console.log('user found : ');
-    console.log(result);
-    return result;
-}
-
-/* ENCRYPTION */
+const chars = "lwZ9Mn0f2tbXLpCK3VNcRBqvJgx4mGHeju6S7ar5DoAyYFT8UkdWzEOsihPQ1I";
 
 function encrypt(pwd, encryptionKey){
-    // pwd is unencrypted
-    // length : 3-16 char's
-    // encrypted pwd length = 20 char's
     const maxLen = 20;
-    //const chars = "lwZ9M{)@n0(f2tbX>!/Lp<CK*]$.?,3_VNcRB#%}qvJgx4&mGH-eju6[S;7ar5D=oAy^YFT8Ukd+WzEOsihPQI";
-    const chars = "lwZ9Mn0f2tbXLpCK3VNcRBqvJgx4mGHeju6S7ar5DoAyYFT8UkdWzEOsihPQ1I";
 
-    let randomIndex = Math.floor(Math.random() * (chars.length));
+    let randomIndex = 0;
 
     let encryptedPwd = pwd;
 
-    // add two char's to start of pwd
+    // add two random char's to start of pwd
     for (let i = 0; i < 2; i++) {
         randomIndex = Math.floor(Math.random() * (chars.length));
         encryptedPwd = chars[randomIndex] + encryptedPwd;  
     }
-    // add two char's to end of pwd
+
+    // add two random char's to end of pwd
     for (let i = 0; i < 2; i++) {
         randomIndex = Math.floor(Math.random() * (chars.length));
         encryptedPwd += chars[randomIndex];  
     }
 
     // keep adding until pwd is 20 char's long
-
-    while( encryptedPwd.length<20 ){
+    while( encryptedPwd.length < maxLen ){
         randomIndex = Math.floor(Math.random() * (chars.length));
         encryptedPwd += chars[randomIndex];
     }
@@ -314,14 +295,12 @@ function encrypt(pwd, encryptionKey){
         index = chars.indexOf(encryptArr[j]);
         index += encryptionKey;
         if( index > chars.length-1 ){
-            console.log('looped index : '+index)
             index -= chars.length;
         }
         if( index < 0 ){
-            console.log('- looped index : '+index)
             index += chars.length;
         }
-        console.log(encryptArr[j]+"==>"+chars[index]);
+        // console.log(encryptArr[j]+"==>"+chars[index]);           // debug
         encryptArr[j] = chars[index];
     }
 
@@ -336,14 +315,12 @@ function encrypt(pwd, encryptionKey){
 
 function decrypt(encryptedPwd, pwdLength, encryptionKey){
 
-    const chars = "lwZ9Mn0f2tbXLpCK3VNcRBqvJgx4mGHeju6S7ar5DoAyYFT8UkdWzEOsihPQ1I";
-
     let decryptedPwd = encryptedPwd.substr(2, pwdLength);
 
     const decryptArr = [...decryptedPwd];
 
     let index = 0;
-    // substitution cypher
+    // reverse substitution cypher
     for(let j = 0; j < decryptArr.length; j++){
         index = chars.indexOf(decryptArr[j]);
         index -= encryptionKey;
@@ -353,7 +330,7 @@ function decrypt(encryptedPwd, pwdLength, encryptionKey){
         if( index > chars.length-1 ){
             index -= chars.length;
         }
-        console.log(decryptArr[j]+"==>"+chars[index]);
+        // console.log(decryptArr[j]+"==>"+chars[index]);       // debug
         decryptArr[j] = chars[index];
     }
 
@@ -366,15 +343,8 @@ function decrypt(encryptedPwd, pwdLength, encryptionKey){
     return decryptedPwd;
 }
 
-// end SAVE GAME
 
-function removeDuplicateUser(){
-    // TODO:
-    // check for duplicate and remove from test db
-    // sometimes duplicates show up in db during save game
-}
-
-// dev functions
+/*** DEV FUNCTIONS ***/
 
 async function resetHighScoreCollection(client){
     let defaultScores = [];
@@ -382,20 +352,15 @@ async function resetHighScoreCollection(client){
         defaultScores.push({playerName:'player unknown',score:0});
     }
     const result_delete = await client.db(DATABASE).collection(COLLECTION_HIGHSCORE).deleteMany({}); // clear database
-    console.log(`${result_delete.deletedCount} documents deleted.`);
     const result = await client.db(DATABASE).collection(COLLECTION_HIGHSCORE).insertMany(defaultScores);
-    console.log(`High Score List Length : ${result.insertedCount}`);
-    //console.log(`ID's : ${result.insertedIds}`);
 }
 
 async function clearAllSaveGameData(client){
-    // clear
     const result_delete1 = await client.db(DATABASE).collection(COLLECTION_SAVE_DATA).deleteMany({});
     const result_delete2 = await client.db(DATABASE).collection(COLLECTION_USER_CREDS).deleteMany({});
 }
 
 async function getAllSaveGameData(client){
-   // Check databases
    const result1 = await client.db(DATABASE).collection(COLLECTION_SAVE_DATA).find({});
    const result2 = await client.db(DATABASE).collection(COLLECTION_USER_CREDS).find({});
    const db1 = await result1.toArray();
@@ -411,89 +376,3 @@ async function getAllSaveGameData(client){
    console.log(db2);
 }
 
-/* test code */
-
-let test_user_db = [
-    {
-        playerName: 'Player1',
-        passphrase: '123',
-    },
-    {
-        playerName: 'Player2',
-        passphrase: 'abc',
-    },
-    {
-        playerName: 'Player3',
-        passphrase: '!@#',
-    },
-]
-
-let test_save_game = [
-    /*
-        format: {
-            playerName: '',
-            date : '',
-            game: '',
-            score: 0,
-            tries: 0,
-            blockMap: [],
-        },
-    */
-        {
-            playerName: 'Player1',
-            // passphrase: '123',
-            date : '',
-            game: '<h2>game data for player <b>ONE</b></h2>',
-            score: 1,
-            tries: 1,
-            blockMap: [],
-        },
-        {
-            playerName: 'Player2',
-            // passphrase: '123',
-            date : '',
-            game: '<h2>game data for player <b>TWO</b></h2>',
-            score: 2,
-            tries: 2,
-            blockMap: [],
-        },
-        {
-            playerName: 'Player3',
-            // passphrase: '123',
-            date : '',
-            game: '<h2>game data for player <b>THREE</b></h2>',
-            score: 3,
-            tries: 3,
-            blockMap: [],
-        },
-];
-
-let sample_data = [
-    {
-        name:   "wolf_1",
-        score:  "123",
-        id:     "01"
-    },
-    {
-        name:   "bear_2",
-        score:  "89",
-        id:     "02"
-    },
-    {
-        name:   "fox_3",
-        score:  "3",
-        id:     "03"
-    },
-    {
-        name:   "snake_4",
-        score:  "153",
-        id:     "04"
-    },
-    {
-        name:   "tiger_5",
-        score:  "13",
-        id:     "05"
-    },
-]
-
-/* end test code */
